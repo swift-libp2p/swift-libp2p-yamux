a6aa65e42484990f238ed233d17e97ea366a6ff7
Brandon <32753167+btoms20@users.noreply.github.com>
2025-12-11 12:28:59 -0800

    added Frame tests

--- Tests/LibP2PYAMUXTests/FrameTests.swift
+++ Tests/LibP2PYAMUXTests/FrameTests.swift
@@ -14,14 +14,17 @@
 
 import LibP2P
 import NIOTestUtils
-import XCTest
+import Testing
 
 @testable import LibP2PYAMUX
 
-final class FrameTests: XCTestCase {
+@Suite("Frame Tests")
+struct FrameTests {
+
+    let defaultWindowSize = UInt32(1024 * 256)
 
     /// Header frame encoding / decoding
-    func testFrameToMessages() throws {
+    @Test func testFrameToMessages() throws {
         // Create a basic header frame
         var payload = ByteBuffer()
         payload.writeString("Hello World!")
@@ -35,9 +38,12 @@ final class FrameTests: XCTestCase {
         let frame = Frame(header: header, payload: payload)
 
         // To messages
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .data(payload: payload), .close]
+        #expect(
+            frame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+            ]
         )
 
         // Serialize
@@ -45,98 +51,106 @@ final class FrameTests: XCTestCase {
         wireBuffer.write(frame: frame)
 
         // Ensure the header encoded within 12 bytes
-        XCTAssertEqual(wireBuffer.readableBytes, 12 + Int(header.length))
+        #expect(wireBuffer.readableBytes == 12 + Int(header.length))
         // Ensure all bytes are 0 for this particular header
-        XCTAssertEqual(
-            wireBuffer.readableBytesView.withUnsafeBytes { Array($0) },
-            [0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 12] + [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
+        #expect(
+            wireBuffer.readableBytesView.withUnsafeBytes { Array($0) } == [0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 12] + [
+                72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33,
+            ]
         )
 
         let recoveredFrame = try wireBuffer.readFrame()
 
-        XCTAssertEqual(recoveredFrame.header, header)
-        XCTAssertEqual(recoveredFrame.payload, payload)
-        XCTAssertEqual(
-            recoveredFrame.messages,
-            [.newStream, .data(payload: payload), .close]
+        #expect(recoveredFrame.header == header)
+        #expect(recoveredFrame.payload == payload)
+        #expect(
+            recoveredFrame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+            ]
         )
-        XCTAssertEqual(wireBuffer.readableBytes, 0)
+        #expect(wireBuffer.readableBytes == 0)
     }
 
     /// Header frame encoding / decoding
-    func testFrameToMessages2() throws {
+    @Test func testFrameToMessages_AddFlags() throws {
         // Create a basic header frame
         let payload = ByteBuffer(string: "Hello World!")
-        let frame = try Frame(streamID: 1, message: .data(payload: payload), additionalFlags: [.syn, .fin])
-
-        // To messages
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .data(payload: payload), .close]
+        //var frame = try Frame(streamID: 1, message: .data(payload: payload))
+        var frame = Frame(
+            header: Header(
+                version: .v0,
+                messageType: .data,
+                flags: [],
+                streamID: 1,
+                length: UInt32(payload.readableBytes)
+            ),
+            payload: payload
         )
-
-        // Serialize
-        var wireBuffer = ByteBuffer()
-        wireBuffer.write(frame: frame)
-
-        // Ensure the header encoded within 12 bytes
-        XCTAssertEqual(wireBuffer.readableBytes, 12 + Int(frame.header.length))
-        // Ensure all bytes are 0 for this particular header
-        XCTAssertEqual(
-            wireBuffer.readableBytesView.withUnsafeBytes { Array($0) },
-            [0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 12] + [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33]
-        )
-
-        let recoveredFrame = try wireBuffer.readFrame()
-
-        XCTAssertEqual(recoveredFrame.header, frame.header)
-        XCTAssertEqual(recoveredFrame.payload, payload)
-        XCTAssertEqual(
-            recoveredFrame.messages,
-            [.newStream, .data(payload: payload), .close]
-        )
-        XCTAssertEqual(wireBuffer.readableBytes, 0)
-    }
-
-    /// Header frame encoding / decoding
-    func testFrameToMessages_AddFlags() throws {
-        // Create a basic header frame
-        let payload = ByteBuffer(string: "Hello World!")
-        var frame = try Frame(streamID: 1, message: .data(payload: payload))
         frame.addFlag(.syn)
         frame.addFlag(.fin)
 
         // To messages
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .data(payload: payload), .close]
+        #expect(
+            frame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+            ]
         )
 
         // Attempt to add a duplicate flag
         frame.addFlag(.syn)
 
         // Ensure we can't add duplicate flags
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .data(payload: payload), .close]
+        #expect(
+            frame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+            ]
         )
 
         // Add another flag
         frame.addFlag(.ack)
 
         // Ensure we can't add duplicate flags
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .openConfirmation, .data(payload: payload), .close]
+        #expect(
+            frame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelOpenConfirmation(
+                    .init(
+                        recipientChannel: 1,
+                        senderChannel: 1,
+                        initialWindowSize: 0,
+                        maximumPacketSize: defaultWindowSize
+                    )
+                ),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+            ]
         )
 
         // Add another flag
         frame.addFlag(.reset)
 
         // Ensure we can't add duplicate flags
-        XCTAssertEqual(
-            frame.messages,
-            [.newStream, .openConfirmation, .data(payload: payload), .close, .reset]
+        #expect(
+            frame.messages == [
+                .channelOpen(.init(senderChannel: 1, initialWindowSize: 0, maximumPacketSize: defaultWindowSize)),
+                .channelOpenConfirmation(
+                    .init(
+                        recipientChannel: 1,
+                        senderChannel: 1,
+                        initialWindowSize: 0,
+                        maximumPacketSize: defaultWindowSize
+                    )
+                ),
+                .channelData(.init(recipientChannel: 1, data: payload)),
+                .channelClose(.init(recipientChannel: 1)),
+                .channelReset(.init(recipientChannel: 1, reasonCode: 0, description: "Stream Reset")),
+            ]
         )
     }
 }
